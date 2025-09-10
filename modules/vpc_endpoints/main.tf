571875fab7cd4e2a7b9603a0e90b21aa6edb6efe
Banti2023 <bantithakuradarsh4@gmail.com>
2025-09-10 15:32:32 +0530

    changes commit

--- modules/vpc_endpoints/main.tf
+++ modules/vpc_endpoints/main.tf
@@ -1,15 +1,20 @@
+##########################
+# S3 Gateway Endpoint
+##########################
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${var.aws_region}.${var.s3_service_name}"
-  vpc_endpoint_type = var.s3_vpc_endpoint_type
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
   route_table_ids   = [var.route_table_private_id, var.route_table_public_id]
-  tags              = { Name = "${var.cluster_name}-${var.s3_service_name}-endpoint" }
+
+  tags = {
+    Name = "${var.cluster_name}-s3-endpoint"
+  }
 }
 
-locals {
-  interface_endpoints = var.interface_endpoints
-}
-
+##########################
+# Security Group for Interface Endpoints
+##########################
 resource "aws_security_group" "endpoints_sg" {
   name        = var.endpoints_sg_name
   description = var.endpoints_sg_description
@@ -34,6 +39,49 @@ resource "aws_security_group" "endpoints_sg" {
   }
 }
 
+##########################
+# Local variable for 33 interface endpoints
+##########################
+locals {
+  interface_endpoints = [
+    "ec2",
+    "ecr.api",
+    "ecr.dkr",
+    "logs",
+    "sts",
+    "ssm",
+    "kms",
+    "cloudwatch",
+    "autoscaling",
+    "elasticloadbalancing",
+    "sns",
+    "sqs",
+    "lambda",
+    "cloudformation",
+    "cloudtrail",
+    "config",
+    "dynamodb",
+    "secretsmanager",
+    "athena",
+    "glue",
+    "elasticache",
+    "rds",
+    "apigateway",
+    "sagemaker",
+    "batch",
+    "elasticfilesystem",
+    "cloudfront",
+    "elasticbeanstalk",
+    "kinesis",
+    "xray",
+    "codebuild",
+    "codedeploy"
+  ]
+}
+
+##########################
+# Interface Endpoints
+##########################
 resource "aws_vpc_endpoint" "interface_endpoints" {
   for_each            = toset(local.interface_endpoints)
   vpc_id              = var.vpc_id
@@ -42,6 +90,7 @@ resource "aws_vpc_endpoint" "interface_endpoints" {
   subnet_ids          = var.private_subnets_ids
   security_group_ids  = [aws_security_group.endpoints_sg.id]
   private_dns_enabled = var.private_dns_enabled
+
   tags = {
     Name = "${var.cluster_name}-${each.key}-endpoint"
   }
--- modules/vpc_endpoints/variables.tf
+++ modules/vpc_endpoints/variables.tf
@@ -16,11 +16,13 @@ variable "cluster_name" {
 variable "s3_service_name" {
   type        = string
   description = "Service name for S3 endpoint"
+  default     = "s3"
 }
 
 variable "s3_vpc_endpoint_type" {
   type        = string
   description = "Type of VPC endpoint for S3"
+  default     = "Gateway"
 }
 
 variable "route_table_private_id" {
@@ -36,56 +38,100 @@ variable "route_table_public_id" {
 variable "interface_endpoints" {
   type        = list(string)
   description = "List of interface endpoints to create"
+  default = [
+    "ec2",
+    "ecr.api",
+    "ecr.dkr",
+    "logs",
+    "sts",
+    "ssm",
+    "kms",
+    "cloudwatch",
+    "autoscaling",
+    "elasticloadbalancing",
+    "sns",
+    "sqs",
+    "lambda",
+    "cloudformation",
+    "cloudtrail",
+    "config",
+    "dynamodb",
+    "secretsmanager",
+    "athena",
+    "glue",
+    "elasticache",
+    "rds",
+    "apigateway",
+    "sagemaker",
+    "batch",
+    "elasticfilesystem",
+    "cloudfront",
+    "elasticbeanstalk",
+    "kinesis",
+    "xray",
+    "codebuild",
+    "codedeploy"
+  ]
 }
 
 variable "endpoints_sg_name" {
   type        = string
   description = "Name of the security group for endpoints"
+  default     = "private-eks-cluster-endpoints-sg"
 }
 
 variable "endpoints_sg_description" {
   type        = string
   description = "Description of the security group for endpoints"
+  default     = "Security group for interface endpoints"
 }
 
 variable "ingress_from_port" {
   type        = number
   description = "Ingress rule from port"
+  default     = 443
 }
 
 variable "ingress_to_port" {
   type        = number
   description = "Ingress rule to port"
+  default     = 443
 }
 
 variable "ingress_protocol" {
   type        = string
   description = "Protocol for ingress rule"
+  default     = "tcp"
 }
 
 variable "ingress_cidr_blocks" {
   type        = list(string)
   description = "CIDR blocks allowed for ingress"
+  default     = ["10.10.0.0/16"]
 }
 
 variable "egress_from_port" {
   type        = number
   description = "Egress rule from port"
+  default     = 0
 }
 
 variable "egress_to_port" {
   type        = number
   description = "Egress rule to port"
+  default     = 0
 }
 
 variable "egress_protocol" {
   type        = string
   description = "Protocol for egress rule"
+  default     = "-1"
 }
 
 variable "egress_cidr_blocks" {
   type        = list(string)
   description = "CIDR blocks allowed for egress"
+  default     = ["0.0.0.0/0"]
 }
 
 variable "private_subnets_ids" {
@@ -96,10 +142,11 @@ variable "private_subnets_ids" {
 variable "interface_vpc_endpoint_type" {
   type        = string
   description = "Type of VPC endpoint for interface endpoints"
+  default     = "Interface"
 }
 
 variable "private_dns_enabled" {
   type        = bool
   description = "Whether private DNS is enabled"
+  default     = true
 }
-
